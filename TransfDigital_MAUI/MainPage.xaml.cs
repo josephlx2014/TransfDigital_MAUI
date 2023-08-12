4e701a74f3846e20a8c54cea3e7f07598602284f
joseph <46848008+josephlx2014@users.noreply.github.com>
2023-08-12 11:44:57 -0600

    Explorando Vertical And Horizontal Layouts

--- TransfDigital_MAUI/MainPage.xaml.cs
+++ TransfDigital_MAUI/MainPage.xaml.cs
@@ -8,18 +8,14 @@
         {
             InitializeComponent();
 
+            //var a = new BoxView { Color = Colors.Silver, HeightRequest = 40 };
+            //var b = new BoxView { Color = Colors.Blue, HeightRequest = 40 };
+            //var c = new BoxView { Color = Colors.Gray, HeightRequest = 40 };
 
+            //stack.Children.Add(a);
+            //stack.Children.Add(b);
+            //stack.Children.Add(c);
 
         }
-
-        void OnHorizontalStartClicked(object sender, EventArgs e) { target.HorizontalOptions = LayoutOptions.Start; }
-        void OnHorizontalCenterClicked(object sender, EventArgs e) { target.HorizontalOptions = LayoutOptions.Center; }
-        void OnHorizontalEndClicked(object sender, EventArgs e) { target.HorizontalOptions = LayoutOptions.End; }
-        void OnHorizontalFillClicked(object sender, EventArgs e) { target.HorizontalOptions = LayoutOptions.Fill; }
-
-        void OnVerticalStartClicked(object sender, EventArgs e) { target.VerticalOptions = LayoutOptions.Start; }
-        void OnVerticalCenterClicked(object sender, EventArgs e) { target.VerticalOptions = LayoutOptions.Center; }
-        void OnVerticalEndClicked(object sender, EventArgs e) { target.VerticalOptions = LayoutOptions.End; }
-        void OnVerticalFillClicked(object sender, EventArgs e) { target.VerticalOptions = LayoutOptions.Fill; }
     }
 }
